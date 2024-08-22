80dfa10c32d5cdcd9f210b0f74f00a204f927b38
Gokul S <149245856+Gokuls2711@users.noreply.github.com>
2024-08-22 17:20:09 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,30 @@
+function myFirstJsCode() {
+  return "Javascript";
+  myFirstJsCode();
+}
+console.log(myFirstJsCode);
+
+var x = 500;
+let y = x + 500;
+y;
+console.log(y);
+
+var number_1 = 8055;
+number = 5055;
+console.log(number_1);
+console.log(number);
+console.log(number_1 * number);
+console.log((number_1 + number) * 2);
+
+var ok_1 = "true";
+var ok_2 = "false";
+console.log(ok_2);
+
+var person = "gokul   ";
+console.log(person.length);
+console.log(person.toUpperCase());
+console.log(person.trim());
+
+var person_1 = "go";
+var person_2 = "kul";
+console.log(person_1 + person_2);
